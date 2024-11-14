dfe9a0a248c757475e3b810a21f730e35825de8c
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:23:51 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "name" {
+  description = "Name of the Kinesis stream."
+  value       = module.kinesis.name
+}
+
+output "shard_count" {
+  description = "Number of shards provisioned."
+  value       = module.kinesis.shard_count
+}
+
+output "stream_arn" {
+  description = "ARN of the the Kinesis stream."
+  value       = module.kinesis.stream_arn
 }
